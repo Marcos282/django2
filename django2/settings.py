b64561dd02aed97d7e53ed34150e2cc5175d780a
marcos <novahost@gmail.com>
2025-08-22 20:24:04 -0300

    segundo teste

--- django2/settings.py
+++ django2/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mp8iuqz&=3av9wxdyi(p7hkdgjg=(uz8zl37ik3oq0&%wd*y0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['novainfo.com.br','www.novainfo.com.br']
 
 
 # Application definition
